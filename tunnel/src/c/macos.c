aeb291c909606f67a3fe0a02232a34c5a5a72a08
Nikhil Unni <nikhil.unni@cortex.io>
2024-08-05 08:10:16 -0700

    WIP utun

--- tunnel/src/c/macos.c
+++ tunnel/src/c/macos.c
@@ -4,15 +4,80 @@
 #include <sys/sys_domain.h>
 #include <sys/kern_control.h>
 #include <net/if_utun.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
 
+int init_interface(const uint8_t tun_id) {
+    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (socket_fd < 0) {
+        perror("socket(AF_INET, SOCK_DGRAM, 0)");
+        return -1;
+    }
+
+    // Get utun name
+    char *tun_name_fmt = "utun%d";
+    int tun_name_len = snprintf(NULL, 0, tun_name_fmt, tun_id);
+
+    struct ifreq ifr;
+    snprintf(ifr.ifr_name, IFNAMSIZ, tun_name_fmt, tun_id);
+
+    fprintf(stdout, "Interface name: %s\n", ifr.ifr_name);
+
+    struct sockaddr_in *addr = (struct sockaddr_in *)&ifr.ifr_addr;
+    addr->sin_family = AF_INET;
+
+    // Set IP address
+    addr->sin_addr.s_addr = inet_addr("10.0.0.1");
+    if (ioctl(socket_fd, SIOCSIFADDR, &ifr) < 0) {
+        perror("ioctl SIOCSIFADDR");
+        close(socket_fd);
+        return -1;
+    }
+
+    // Set point-to-point address
+    addr->sin_addr.s_addr = inet_addr("10.0.0.2");
+    if (ioctl(socket_fd, SIOCSIFDSTADDR, &ifr) < 0) {
+        perror("ioctl SIOCSIFDSTADDR");
+        close(socket_fd);
+        return -1;
+    }
+
+    // Set netmask
+    addr->sin_addr.s_addr = inet_addr("255.255.255.0");
+    if (ioctl(socket_fd, SIOCSIFNETMASK, &ifr) < 0) {
+        perror("ioctl SIOCSIFNETMASK");
+        close(socket_fd);
+        return -1;
+    }
+
+    // Bring the interface up
+    ifr.ifr_flags |= IFF_UP;
+    if (ioctl(socket_fd, SIOCSIFFLAGS, &ifr) < 0) {
+        perror("ioctl SIOCSIFFLAGS");
+        close(socket_fd);
+        return -1;
+    }
+
+    close(socket_fd);
+    return 0;
+}
+
 int open_utun(uint8_t* tun_id) {
     // Open socket for use
     int socket_fd = socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL);
 
+    if (socket_fd < 0) {
+        perror("socket(PF_SYSTEM, SOCK_DGRAM, SYSPROTO_CONTROL)");
+        return -1;
+    }
+
+    // Open utun
     struct ctl_info info;
 
     if (strlcpy(info.ctl_name, UTUN_CONTROL_NAME, sizeof(info.ctl_name)) >= sizeof(info.ctl_name)) {
@@ -34,6 +99,7 @@ int open_utun(uint8_t* tun_id) {
 
     int err = 0;
 
+    // Keep trying to connect to utun{tun_id} until we find an available one
     do {
         addr.sc_unit = *tun_id + 1;
         err = connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
@@ -43,10 +109,18 @@ int open_utun(uint8_t* tun_id) {
         }
     } while (err < 0 && *tun_id < 255);
 
-    if (err != 0) {
+    if (err < 0) {
         close(socket_fd);
         return -1;
-    } else {
-        return socket_fd;
     }
+
+    err = init_interface(*tun_id);
+
+    if (err < 0) {
+        fprintf(stderr, "Failed to initialize interface\n");
+        close(socket_fd);
+        return -1;
+    }
+
+    return socket_fd;
 }
--- tunnel/src/lib.rs
+++ tunnel/src/lib.rs
@@ -1,7 +1,10 @@
+use std::fs::File;
 use std::io;
+use std::net::UdpSocket;
+use std::os::fd::FromRawFd;
 
 #[cfg(target_os = "macos")]
-pub fn open_tunnel() -> io::Result<(i32, String)> {
+pub fn open_tunnel() -> io::Result<(File, String)> {
     let mut tunnel_id: u8 = 0;
     let fd = unsafe { open_utun(&mut tunnel_id) };
     if fd < 0 {
@@ -9,8 +12,9 @@ pub fn open_tunnel() -> io::Result<(i32, String)> {
     }
 
     let tunnel_name = format!("utun{}", tunnel_id);
+    let socket = unsafe { File::from_raw_fd(fd) };
 
-    Ok((fd, tunnel_name))
+    Ok((socket, tunnel_name))
 }
 
 #[cfg(target_os = "macos")]
@@ -20,18 +24,93 @@ extern "C" {
 
 #[cfg(test)]
 mod tests {
-    use std::thread;
+    use std::{io, thread};
+    use std::io::{Read, Write};
+
+    use nix::sys::socket::{socket, AddressFamily, SockType, SockFlag};
+    use nix::sys::socket::{connect, SockaddrIn};
+    use std::os::unix::io::RawFd;
+    use std::ffi::CString;
+    use std::mem;
+    use std::os::fd::OwnedFd;
 
     use crate::open_tunnel;
 
     #[cfg(target_os = "macos")]
     #[test]
-    fn test() {
-        let (fd, name) = open_tunnel().expect("Failed to open tunnel");
+    fn test() -> io::Result<()> {
+        let (mut file, name) = open_tunnel()?;
         println!("Connected to tunnel: {}", name);
 
+        let default_device_name = pcap::Device::lookup().unwrap().unwrap().name;
+        println!("Forwarding requests to {}", &default_device_name);
+
+        let mut cap = pcap::Capture::from_device("en0")
+            .unwrap()
+            .timeout(100)
+            .promisc(true)
+            .open()
+            .unwrap();
+
+        let datalink_type = cap.get_datalink().0;
+        println!("Data link type: {}", datalink_type);
+
+        let mut buffer = [0u8; 65_535];
         loop {
-            thread::sleep(std::time::Duration::from_secs(10));
+            match file.read(&mut buffer) {
+                Ok(bytes_received) => {
+                    // println!("Received {} bytes", bytes_received);
+                    println!("Request: {:02x?}", &buffer[..bytes_received]);
+
+                    match cap.sendpacket(&buffer[..bytes_received]) {
+                        Ok(_) => {
+                            // println!("Successfully sent packet to {}", &default_device_name);
+                        },
+                        Err(e) => {
+                            eprintln!("Dropped packet sending to {}: {}", &default_device_name, e);
+                            continue;
+                        },
+                    }
+
+                    match cap.next_packet() {
+                        Ok(packet) => {
+                            println!("Response: {:02x?}", &packet.data);
+                            file.write_all(&packet.data)?;
+                        },
+                        Err(e) => {
+                            eprintln!("Dropped packet receiving from {}: {}", &default_device_name, e);
+                        },
+                    }
+
+                    // file.write(&buffer[..bytes_received])?;
+                    // file.write("wassup".as_bytes())?;
+                    // println!("{:#04x?}", &buffer[0..49]);
+                }
+                Err(e) => {
+                    eprintln!("Error: {}", e);
+                }
+            }
         }
+    }
+
+    #[test]
+    fn open_device() -> io::Result<()> {
+        let default_device_name = pcap::Device::lookup().unwrap().unwrap();
+
+        println!("Starting...");
+
+        let mut cap = pcap::Capture::from_device("utun7")
+            .unwrap()
+            .timeout(100)
+            .promisc(true)
+            .open()
+            .unwrap();
+
+        while let Ok(packet) = cap.next_packet() {
+            println!("Packet length: {}", packet.len());
+            // println!("{:?}", packet.data);
+        }
+
+        Ok(())
     }
 }
